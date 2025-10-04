f4f775efedb60ed8ce1f06a5c35e66b8d677a36f
Abdaland <Abdallah-sonic@hotmail.fr>
2025-10-04 20:04:41 +0100

    Fix: moved task files back to programming_paradigm folder

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+class BankAccount:
+    """A simple Bank Account class to demonstrate OOP concepts."""
+
+    def __init__(self, initial_balance=0):
+        self.__account_balance = initial_balance  # encapsulated attribute
+
+    def deposit(self, amount):
+        """Add money to the account."""
+        if amount > 0:
+            self.__account_balance += amount
+
+    def withdraw(self, amount):
+        """Withdraw money if sufficient funds exist."""
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        """Display the current balance."""
+        print(f"Current Balance: ${self.__account_balance}")
